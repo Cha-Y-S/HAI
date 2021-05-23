fbed165aa26a88d10916e1b9d353fe212bf67c1d
Cha_Y_S <vaite714@gmail.com>
2021-05-24 06:29:29 +0900

    Commit for Hosting

---
+++ FacialLandmarkDetection/build/precache-manifest.d9f5a18b441c0e84e111d36cd77c487c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877c31f7ba6bbc14c9a4dfc300a693b7",
+    "url": "/HAI/index.html"
+  },
+  {
+    "revision": "0161350649b99f4b5136",
+    "url": "/HAI/static/css/main.59eb4992.chunk.css"
+  },
+  {
+    "revision": "7261d8bbf19627e5f277",
+    "url": "/HAI/static/js/2.03d9f2c3.chunk.js"
+  },
+  {
+    "revision": "cf1b385efbc1010ade688c7fac51d128",
+    "url": "/HAI/static/js/2.03d9f2c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0161350649b99f4b5136",
+    "url": "/HAI/static/js/main.0426b9b4.chunk.js"
+  },
+  {
+    "revision": "8e58be0f16ebcd5424db",
+    "url": "/HAI/static/js/runtime-main.94cf5f1f.js"
+  },
+  {
+    "revision": "19fb8872512560bb846464e91ae44bbd",
+    "url": "/HAI/static/media/1.19fb8872.jpg"
+  },
+  {
+    "revision": "3ab4cc180f955f73cd076041f419a325",
+    "url": "/HAI/static/media/faceSample.3ab4cc18.png"
+  },
+  {
+    "revision": "183e3b65d7c3390bacbce606601c6590",
+    "url": "/HAI/static/media/faceSample3.183e3b65.png"
+  },
+  {
+    "revision": "c19fa1989708a9bddd2b53d8de837d47",
+    "url": "/HAI/static/media/manCirNRe1.c19fa198.jpg"
+  },
+  {
+    "revision": "5364d738f714b14c70cbaf78c1ed761b",
+    "url": "/HAI/static/media/manCirRe2.5364d738.jpg"
+  },
+  {
+    "revision": "e76dc70ca1183a550faa3869edab846e",
+    "url": "/HAI/static/media/manRecNRe1.e76dc70c.jpg"
+  },
+  {
+    "revision": "f2ee1dfc24f04c5a612ebd5897b166ba",
+    "url": "/HAI/static/media/manRecRe1.f2ee1dfc.jpg"
+  },
+  {
+    "revision": "eb4b604d8fa649e00126d7e64a4bfe3a",
+    "url": "/HAI/static/media/manTriRe2.eb4b604d.jpg"
+  },
+  {
+    "revision": "56fd9c949e66553bf68c24e7699740fe",
+    "url": "/HAI/static/media/womanCirNRe1.56fd9c94.jpg"
+  },
+  {
+    "revision": "5596c368c7329a8bc4efdefbe065ff6d",
+    "url": "/HAI/static/media/womanCirNRe2.5596c368.jpg"
+  },
+  {
+    "revision": "21334f550ab5ad0ffc272d2e5a64d899",
+    "url": "/HAI/static/media/womanCirRe1.21334f55.jpg"
+  },
+  {
+    "revision": "8c75809c788a41a3413b60b90eba09ec",
+    "url": "/HAI/static/media/womanEggRe1.8c75809c.jpg"
+  },
+  {
+    "revision": "ea3253bb847f9857df5063ebb844bbe7",
+    "url": "/HAI/static/media/womanRecNRe1.ea3253bb.jpg"
+  },
+  {
+    "revision": "503ad24fbbfbdd31357f3948150e1bc0",
+    "url": "/HAI/static/media/womanRecNRe2.503ad24f.jpg"
+  },
+  {
+    "revision": "1b3a2f76c2de0ef9db746499f99ce63b",
+    "url": "/HAI/static/media/womanRecRe1.1b3a2f76.jpg"
+  },
+  {
+    "revision": "a885f6debc5c85c57ee60ca2f59cc379",
+    "url": "/HAI/static/media/womanRecRe2.a885f6de.jpg"
+  },
+  {
+    "revision": "705055c4fe8e355a15228f55bacbbb9f",
+    "url": "/HAI/static/media/womanTriNRe1.705055c4.jpg"
+  },
+  {
+    "revision": "9c9fbcf53f5ce460ca769e4430601a5a",
+    "url": "/HAI/static/media/womanTriNRe2.9c9fbcf5.jpg"
+  },
+  {
+    "revision": "454c1352fcfdf7857061b0f188a1a76f",
+    "url": "/HAI/static/media/womanTriRe1.454c1352.jpg"
+  },
+  {
+    "revision": "1f3b67ecdf7608cc0390ef7d62937f7e",
+    "url": "/HAI/static/media/womanTriRe2.1f3b67ec.jpg"
+  }
+]);
